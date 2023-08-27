7e0625acd5111caf2e26969d174baaf274bf772f
Terry Mace <terence.h.mace@gmail.com>
2023-08-27 18:23:06 +0100

    Set up logging

--- src/CompanyDetails.Api/Controllers/CompanyController.cs
+++ src/CompanyDetails.Api/Controllers/CompanyController.cs
@@ -6,11 +6,20 @@ namespace CompanyDetails.Api.Controllers;
 [Route("v{version:apiVersion}/[controller]")]
 public class CompanyController : ControllerBase
 {
+    private readonly ILogger<CompanyController> _logger;
+
+    public CompanyController(ILogger<CompanyController> logger)
+    {
+        _logger = logger;
+    }
+    
     [HttpGet]
     [ApiVersion("1.0")]
     [Route("{jurisdictionCode}/{companyNumber}")]
     public IActionResult Index(string jurisdictionCode, string companyNumber)
     {
+        _logger.LogError("This is an error");
+        
         return Ok();
     }
 }
--- src/CompanyDetails.Api/Program.cs
+++ src/CompanyDetails.Api/Program.cs
@@ -1,8 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
 
 builder.Services.AddControllers();
 builder.Services.AddApiVersioning(options =>
@@ -15,6 +18,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseSerilogRequestLogging();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
