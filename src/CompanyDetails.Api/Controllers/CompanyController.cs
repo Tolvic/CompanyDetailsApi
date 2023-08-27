467b4c3004e879650c2d4b4ea47776519053fa50
Terry Mace <terence.h.mace@gmail.com>
2023-08-27 17:26:44 +0100

    Set up controller with versioning

---
+++ src/CompanyDetails.Api/Controllers/CompanyController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyDetails.Api.Controllers;
+
+[ApiController]
+[Route("v{version:apiVersion}/[controller]")]
+public class CompanyController : ControllerBase
+{
+    [HttpGet]
+    [ApiVersion("1.0")]
+    [Route("{jurisdictionCode}/{companyNumber}")]
+    public IActionResult Index(string jurisdictionCode, string companyNumber)
+    {
+        return Ok();
+    }
+}
--- src/CompanyDetails.Api/Program.cs
+++ src/CompanyDetails.Api/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddApiVersioning(options =>
+{
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+    options.AssumeDefaultVersionWhenUnspecified = true;
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
